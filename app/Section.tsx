1148ac4221cbb4eb65a33d50989ec628bfbdd0da
19qm <abolfazl@gmail.com>
2023-07-08 10:02:09 +0330

    second commit

--- app/Section.tsx
+++ app/Section.tsx
@@ -9,8 +9,8 @@ export const Section: VFC<ISection> = ({logo, title, description}) => {
     return (
         <section className={"mt-60"}>
             <span className={"flex justify-center"} ><img width={50} height={50} src={logo ? logo : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png"} alt="logo"/></span>
-            <h4 className={"text-amber-700 w-44 text-center mx-auto mt-3  text-3xl"}>100 Years of tradition</h4>
-            <p className={"w-3/6 mx-auto text-center mt-12 text-zinc-400 font-medium"}>Lorem ipsum dolor sit amet,ebitis ducimus, est explicabo ipsam ipsum iste molestiae quae quidem repudiandae soluta tempore ut vero voluptatibus! Laborum quidem sed tempore.</p>
+            <h4 className={"text-amber-700 w-44 text-center mx-auto mt-5  text-3xl"}>{title}</h4>
+            <p className={"lg:w-3/6 md:w-4/6 w-5/6 mx-auto text-center mt-12 text-zinc-400 font-medium"}>{description}</p>
         </section>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import {Section} from "@/app/Section";
 export default function Home() {
     return (
         <div className={""}>
-            <Section logo={""} title={""} description={""} />
+            <Section logo={""} title={"100 Years of tradition"} description={"Lorem ipsum dolor sit amet,ebitis ducimus, est explicabo ipsam ipsum iste molestiae quae quidem repudiandae soluta tempore ut vero voluptatibus! Laborum quidem sed tempore."} />
         </div>
     )
 }
