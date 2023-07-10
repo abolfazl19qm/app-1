649af5609ebb5d32f91f7f34086ec0e4a89662bd
19qm <abolfazl@gmail.com>
2023-07-11 01:04:03 +0330

    update Slider

--- app/SectionSlider.tsx
+++ app/SectionSlider.tsx
@@ -1,10 +1,180 @@
 "use client"
-import {Swiper} from 'swiper/react';
 import {VFC} from "react";
-interface ISection {
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {useEffect, useRef, useState} from "react";
+import 'swiper/css';
+
+interface ISlider {
+    img1: string
+    title1: string
+    volume1: string
+    btnText1: string
+    description1: string
+    price1: string
+    img2: string
+    title2: string
+    volume2: string
+    btnText2: string
+    description2: string
+    price2: string
+    img3: string
+    title3: string
+    volume3: string
+    btnText3: string
+    description3: string
+    price3: string
+    img4: string
+    title4: string
+    volume4: string
+    btnText4: string
+    description4: string
+    price4: string
+
 
 }
-interface ISection {
+
+export const SectionSlider: VFC<ISlider> = ({
+                                                img1,
+                                                title1,
+                                                volume1,
+                                                btnText1,
+                                                description1,
+                                                price1,
+                                                img2,
+                                                title2,
+                                                volume2,
+                                                btnText2,
+                                                description2,
+                                                price2,
+                                                img3,
+                                                title3,
+                                                volume3,
+                                                btnText3,
+                                                description3,
+                                                price3,
+                                                img4,
+                                                title4,
+                                                volume4,
+                                                btnText4,
+                                                description4,
+                                                price4,
+                                            }) => {
+    const swiperRef: any = useRef();
+    const [btnColor, setBtnColor] = useState({
+        btn1: "text-zinc-400",
+        btn2: "text-zinc-400",
+        btn3: "text-zinc-400",
+        btn4: "text-zinc-400",
+    })
+    const btn1 = () => {
+        swiperRef.current.slideTo(0);
+        setBtnColor({
+            btn1: "text-[#CA8F65]",
+            btn2: "text-zinc-400",
+            btn3: "text-zinc-400",
+            btn4: "text-zinc-400",
+        });
+    }
+    const btn2 = () => {
+        swiperRef.current.slideTo(1);
+        setBtnColor({
+            btn1: "text-zinc-400",
+            btn2: "text-[#CA8F65]",
+            btn3: "text-zinc-400",
+            btn4: "text-zinc-400",
+        });
+    }
+    const btn3 = () => {
+        swiperRef.current.slideTo(2);
+        setBtnColor({
+            btn1: "text-zinc-400",
+            btn2: "text-zinc-400",
+            btn3: "text-[#CA8F65]",
+            btn4: "text-zinc-400",
+        });
+    }
+    const btn4 = () => {
+        swiperRef.current.slideTo(3);
+        setBtnColor({
+            btn1: "text-zinc-400",
+            btn2: "text-zinc-400",
+            btn3: "text-zinc-400",
+            btn4: "text-[#CA8F65]",
+        });
+    }
+
+    useEffect(() => {
+        async function Run(list: Function[]) {
+
+            const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+            while (true) {
+                for (const item of list) {
+                    item();
+                    await delay(6000);
+                }
+            }
+        }
+
+        Run([btn1, btn2, btn3, btn4]);
+    }, []);
+    return (
+        <div className={"relative mt-5 md:mt-0"}>
+            <Swiper
+                slidesPerView={1}
+                loop={true}
+                onSwiper={(swiper) => {
+                    swiperRef.current = swiper;
+                }}
+            >
+                <SwiperSlide>
+                    <div><Slide title={title1} img={img1} btnText={btnText1} price={price1} description={description1}
+                                volume={volume1}/></div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div><Slide title={title2} img={img2} btnText={btnText2} price={price2} description={description2}
+                                volume={volume2}/></div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div><Slide title={title3} img={img3} btnText={btnText3} price={price3} description={description3}
+                                volume={volume3}/></div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div><Slide title={title4} img={img4} btnText={btnText4} price={price4} description={description4}
+                                volume={volume4}/></div>
+                </SwiperSlide>
+
+            </Swiper>
+            <div
+                className={" md:left-5 md:w-10 h-[40%] lg:top-44 md:top-28  md:z-10  md:absolute z-20   md:flex-col   justify-evenly flex"}>
+
+
+                <button onClick={() => {
+                    btn1()
+                }}
+                        className={`${btnColor.btn1} transition-all duration-500 hover:text-orange-400 mx-auto  md:w-3/6 md:h-full w-8 rounded-full h-8 bg-zinc-500 md:bg-transparent text-xl`}>01
+                </button>
+                <button onClick={() => {
+                    btn2()
+                }}
+                        className={`${btnColor.btn2} transition-all duration-500 hover:text-orange-400 mx-auto  md:w-3/6 md:h-full w-8 rounded-full h-8 bg-zinc-500 md:bg-transparent text-xl`}>02
+                </button>
+                <button onClick={() => {
+                    btn3()
+                }}
+                        className={`${btnColor.btn3} transition-all duration-500 hover:text-orange-400  mx-auto  md:w-3/6 md:h-full w-8 rounded-full h-8 bg-zinc-500 md:bg-transparent text-xl`}>03
+                </button>
+                <button onClick={() => {
+                    btn4()
+                }}
+                        className={`${btnColor.btn4} transition-all duration-500 hover:text-orange-400  mx-auto  md:w-3/6 md:h-full w-8 rounded-full h-8 bg-zinc-500 md:bg-transparent text-xl`}>04
+                </button>
+            </div>
+        </div>
+    );
+};
+
+interface ISlide {
     title: string
     volume: string
     description: string
@@ -12,58 +182,35 @@ interface ISection {
     price: string
     img: string
 }
-export const SectionSlider: VFC<ISection> = ({img, title, volume, btnText, description, price }) => {
+
+const Slide: VFC<ISlide> = ({img, title, volume, btnText, description, price}) => {
+
+
     return (
-        <section dir={"rtl"} className={"w-[90%] grid mt-96 grid-cols-9 grid-rows-[9] md:grid-rows-1  mx-auto gap-5 "}>
+        <section dir={"rtl"} className={"w-[90%] grid  grid-cols-9 grid-rows-[9] md:grid-rows-1  mx-auto gap-5 "}>
 
             <div className={"w-full md:col-span-4 col-span-9 row-span-3 md:row-span-1  md:h-full"}>
-                <ProductTitle title={title} volume={volume} description={description} price={price} btnText={btnText}/>
+                <div dir={"rtl"} className={"w-[80%]  xl:w-[65%] md:mt-10 xl:mt-20 mx-auto"}>
+                    <h2 className={"text-center md:text-right text-5xl xl:text-7xl text-[#CA8F65]"}>{title}</h2>
+                    <p className={"text-center md:text-right mt-10 lg:mt-12 xl:mt-14 text-orange-100"}>{volume}</p>
+                    <p className={"text-center md:text-right xl:text-xl text-zinc-400 font-medium mt-10 hidden md:block"}>{description}</p>
+                    <div className={"flex items-center mt-10 lg:mt-16 justify-between"}><p
+                        className={"text-[#CA8F65] font-medium"}>{price}</p>
+                        <button
+                            className={"text-[#CA8F65] transition-all duration-300 w-28 h-12 border-2 md:hover:border-orange-100 md:hover:text-orange-100 border-[#CA8F65]"}>{btnText}
+                        </button>
+                        <div className={"hidden md:block"}></div>
+                    </div>
+                </div>
             </div>
+            <div className={"md:col-span-1 col-span-9 hidden lg:block row-span-5 md:row-span-1"}></div>
             <div className={"w-full md:col-span-4 col-span-9 row-span-5 md:row-span-1  md:h-full"}>
-                <img className={"md:w-[80%] mt-10 mx-auto h-3/5 md:h-full xl:h-[90%]"}
+                <img className={"md:w-[80%] xl:w-full mt-10 mx-auto h-96 md:h-96 xl:h-[650px]"}
                      src={img}
                      alt="DRINK"/>
-                {/*<div className={"w-full h10  md:hidden "}>
-                    <ul className={"flex justify-evenly"}>
-                        <li className={"w-4 h-4 rounded-full  border-[2px] border-[#CA8F65]"}><button className={"w-full h-full"}></button></li>
-                        <li className={"w-4 h-4 rounded-full  border-[2px] border-[#CA8F65]"}><button className={"w-full h-full"}></button></li>
-                        <li className={"w-4 h-4 rounded-full  bg-[#CA8F65] border-[2px] border-[#CA8F65]"}><button className={"w-full h-full"}></button></li>
-                        <li className={"w-4 h-4 rounded-full  border-[2px] border-[#CA8F65]"}><button className={"w-full h-full"}></button></li>
-                    </ul>
-                </div>*/}
             </div>
-            {/*<div className={"w-full hidden md:flex-col justify-evenly md:col-span-1 col-span-9 row-span-1 md:row-span-1 md:ml-8 md:flex"}>
-                <button className={"text-zinc-400 w-3/6 mx-auto font-medium text-xl"}>01</button>
-                <button className={"text-zinc-400 w-3/6 mx-auto font-medium text-xl"}>02</button>
-                <button className={"text-[#CA8F65] w-3/6 mx-auto font-medium text-xl"}>03</button>
-                <button className={"text-zinc-400 w-3/6 mx-auto font-medium text-xl"}>04</button>
-            </div>*/}
         </section>
     )
 }
 
-interface Product {
-    title: string
-    volume: string
-    description: string
-    btnText: string
-    price: string
 
-
-}
-
-const ProductTitle: VFC<Product>  = ({title, volume, btnText, description, price}) => {
-    return (
-        <div dir={"rtl"} className={"w-[80%]  xl:w-[55%] md:mt-10 xl:mt-20 mx-auto"}>
-            <h2 className={"text-center md:text-right text-5xl text-[#CA8F65]"}>{title}</h2>
-            <p className={"text-center md:text-right mt-10 text-orange-100"}>{volume}</p>
-            <p className={"text-center md:text-right text-zinc-400 font-medium mt-10 hidden md:block"}>{description}</p>
-            <div className={"flex items-center mt-10 justify-between"}><p
-                className={"text-[#CA8F65] font-medium"}>{price}</p>
-                <button
-                    className={"text-[#CA8F65] transition-all duration-300 w-28 h-12 border-2 md:hover:border-orange-100 md:hover:text-orange-100 border-[#CA8F65]"}>{btnText}
-                </button>
-            </div>
-        </div>
-    )
-}
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,16 @@ import {SectionLogo} from "@/app/SectionLogo";
 import {SectionProduct} from "@/app/SectionProduct";
 import {SectionEvent} from "@/app/SectionEvent";
 import {SectionSlider} from "@/app/SectionSlider";
-import App from "@/app/None";
 
 
-const Section1 = {
+const sectionLogo = {
     textColor: "text-[#CA8F65]",
     logo: "",
     title: "100 Years of tradition",
     description: "Lorem ipsum dolor sit amet,ebitis ducimus, est explicabo ipsam ipsum iste molestiae quae quidem repudiandae soluta tempore ut vero voluptatibus! Laborum quidem sed tempore."
 
 }
-const Section2 = {
+const sectionProduct = {
     description2: " ساختگی ب نامفهوم از صنعت چاپ و با استفاده از که لازم است",
     description3: "طراحان گرافیک است چاپگرها و متون بلکه روزنامه  سطرآنچنان ",
     description1: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ",
@@ -25,7 +24,7 @@ const Section2 = {
     imageCenter: ""
 
 }
-const Section3 = {
+const sectionEvent = {
     title: " ساختگی با تولید سادگی نامفهوم تولید ",
     eventTitle: "پیشنهاد در تاریخ منقضی می شود :",
     btnText: "خرید",
@@ -33,8 +32,35 @@ const Section3 = {
     image: "",
     days: 0,
     hours: 0,
-    minutes: 0,
+    minutes: 1,
     seconds: 0
+
+}
+const sectionSlider = {
+    title1: "ویسکی آبس",
+    img1: "https://themes.muffingroup.com/be/whiskey2/wp-content/uploads/2021/12/whiskey2-slider-pic4.webp",
+    volume1: "300ML|TH",
+    description1: "آبجو در حجم ۵۰۰ سی سی محاسبه شده است و اگر حجم آن بیشتر باشد به همان نسبت به  ارزش آن اضافه گردد.",
+    price1: "تومان80,000",
+    btnText1: "خرید",
+    title2: "ویسکی بلک",
+    img2: "https://themes.muffingroup.com/be/whiskey2/wp-content/uploads/2021/12/whiskey2-slider-pic5.webp",
+    volume2: "700ML|IRAN",
+    description2: "آبجو در حجم ۵۰۰ سی سی محاسبه شده است و اگر حجم آن بیشتر باشد به همان نسبت به  ارزش آن اضافه گردد.",
+    price2: "تومان420,000",
+    btnText2: "خرید",
+    title3: "ویسکی تیچ",
+    img3: "https://themes.muffingroup.com/be/whiskey2/wp-content/uploads/2021/12/whiskey2-slider-pic6.webp",
+    volume3: "700ML|US",
+    description3: "آبجو در حجم ۵۰۰ سی سی محاسبه شده است و اگر حجم آن بیشتر باشد به همان نسبت به  ارزش آن اضافه گردد.",
+    price3: "تومان100,000",
+    btnText3: "خرید",
+    title4: "ویسکی انکو",
+    img4: "https://themes.muffingroup.com/be/whiskey2/wp-content/uploads/2021/12/whiskey2-slider-pic7.webp",
+    volume4: "1500ML|IRLAND",
+    description4: "آبجو در حجم ۵۰۰ سی سی محاسبه شده است و اگر حجم آن بیشتر باشد به همان نسبت به  ارزش آن اضافه گردد.",
+    price4: "تومان690,000",
+    btnText4: "خرید",
 
 }
 export default function Home() {
@@ -42,17 +68,10 @@ export default function Home() {
         <div className={"bg-zinc-800"}>
             <br/>
             <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <SectionLogo {...Section1} />
-            <SectionProduct {...Section2} />
-            <SectionEvent {...Section3}/>
-            <App/>
-            <br/>
+            <SectionLogo {...sectionLogo} />
+            <SectionSlider {...sectionSlider} />
+            <SectionProduct {...sectionProduct} />
+            <SectionEvent {...sectionEvent}/>
             <br/>
             <br/>
             <br/>
